2564f9b11e9a57327aace9a6a35fed21c153921a
FutureNoogler <futureg412@gmail.com>
2023-01-27 00:59:23 +0000

    BUG: unable to store todos in local storage with useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, {useRef, useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from 'uuid'
+
+const LOCAL_STORAGE_KEY = "HELLO WORLD"
 
 function App() {
   const [todos,setTodos] = useState([{id: 1,name: 'Todo 1', completed: false}])
   const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos){ setTodos(storedTodos) }
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos))
+  },[todos])
+
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.completed = !todo.completed
+    setTodos(newTodos)
+  }
 
   function handleAddTodo(e){
     const name = todoNameRef.current.value
@@ -21,13 +39,18 @@ function App() {
     todoNameRef.current.value = null
   }
 
+  function handleClearTodos(){
+    const newTodos = todos.filter(todo => todo.completed)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text"></input>
       <button onClick={handleAddTodo} >Add Todo</button>
-      <button>Clear Complete</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodos}>Clear Complete</button>
+      <div>{todos.filter(todo => !todo.completed).length} left to do</div>
     </>
   )
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export default function Todo({todo}) {
+export default function Todo({todo, toggleTodo}) {
+    function handleClickTodo(){
+        toggleTodo(todo.id)
+    }
+
     return (
         <div>
             <label>
-                <input type="checkbox" checked={todo.completed}></input>
+                <input type="checkbox" checked={todo.completed} onChange={handleClickTodo}></input>
                 {todo.name}
             </label>
         </div>
